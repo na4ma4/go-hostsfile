77fda2ba0dd5d71e6429cec6c66cceb11f9739c3
na4ma4 <na4ma4@users.noreply.github.com>
2020-09-24 14:47:19 +1000

    updated test/main.go to the same code as README.md example

--- test/main.go
+++ test/main.go
@@ -7,18 +7,11 @@ import (
 )
 
 func main() {
-	hosts := map[string][]string{}
 	cb := func(ip, h string) {
-		hosts[ip] = append(hosts[ip], h)
+		log.Printf("'%s': '%s'", ip, h)
 	}
 
 	if err := hostsfile.ParseHostsFile("test/hostfile", cb); err != nil {
 		log.Fatal(err)
-	}
-
-	for ip, hl := range hosts {
-		for _, h := range hl {
-			log.Printf("'%s': '%s'", ip, h)
-		}
 	}
 }
